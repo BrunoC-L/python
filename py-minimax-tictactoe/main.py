c8d4d852911f1fc27e9042664c90ae05951372af
Bruno Curzi-Laliberte <bruno.bcl@sympatico.ca>
2020-02-25 14:50:04 -0500

    refactored and added automove() possibility for odd moves (AI can play first)

--- py-minimax-tictactoe/main.py
+++ py-minimax-tictactoe/main.py
@@ -1,134 +1,80 @@
-import tkinter
-
 def getWinner():
-    win = 0
-    for row in range (0, 3):
-        if (board[row][0] == board[row][1] and board[row][0]== board[row][2] and board[row][0] != 0):
-            return board[row][0]
-    for col in range (0, 3):
-        if (board[0][col] == board[1][col] and board[0][col]== board[2][col] and board[0][col] != 0):
-            return board[0][col]
-    if ((board[0][0] == board[1][1] and board[0][0] == board[2][2] or board[0][2] == board[1][1] and board[0][2] == board[2][0]) and board[1][1] != 0):
-        return board[1][1]
-    return None
+    if ((board[0][0]==board[1][1] and board[0][0]==board[2][2] or board[0][2]==board[1][1] and board[0][2]==board[2][0]) and board[1][1]!=0): return board[1][1]
+    for rowOrCol in range(3):
+        if (board[rowOrCol][0]==board[rowOrCol][1] and board[rowOrCol][0]==board[rowOrCol][2] and board[rowOrCol][0]!=0): return board[rowOrCol][0]
+        if (board[0][rowOrCol]==board[1][rowOrCol] and board[0][rowOrCol]==board[2][rowOrCol] and board[0][rowOrCol]!=0): return board[0][rowOrCol]
 
 def click(event):
-    global movenum
-    if (movenum == 9 or getWinner() != None):
-        return
+    if (movenum == 9 or getWinner() != None): return
     x, y = int(3 * event.x / window.winfo_width()), int(3 * event.y / window.winfo_height())
     if (board[x][y] == 0):
         move(x, y)
         autoMove()
 
 def move(x, y):
-    if (x > 2 or x < 0 or y  > 2 or y < 0):
-        print("Invalid move lol")
+    print(x,y)
     global movenum
     X(x,y) if (movenum % 2 == 0) else O(x,y)
     movenum += 1
 
 def X(x, y):
     board[x][y] = 1
-    canvas.create_line(25 + 100 * x, 25 + 100 * y, 75 + 100 * x, 75 + 100 * y)
-    canvas.create_line(25 + 100 * x, 75 + 100 * y, 75 + 100 * x, 25 + 100 * y)
+    canvas.create_line(50 + 200 * x, 50 + 200 * y, 150 + 200 * x, 150 + 200 * y)
+    canvas.create_line(50 + 200 * x, 150 + 200 * y, 150 + 200 * x, 50 + 200 * y)
 
 def O(x, y):
     board[x][y] = -1
-    canvas.create_oval((25 + 100 * x), (25 + 100 * y), (75 + 100 * x), (75 + 100 * y))
+    canvas.create_oval((50 + 200 * x), (50 + 200 * y), (150 + 200 * x), (150 + 200 * y))
 
-def minimax(maximize):
+def minimax():
     global movenum
-    if (getWinner() != None):
-        return getWinner()
-    if (movenum == 9):
-        return 0
-    if (maximize):
-        value = -10
-        for x in range(3):
-            for y in range(3):
-                if (board[x][y] == 0):
-                    board[x][y] = 1
-                    movenum += 1
-                    value = max(value, minimax(False))
-                    board[x][y] = 0
-                    movenum -= 1
-                    if (value == 1):
-                        return value
-        return value
-    else:
-        value = 10
-        for x in range(3):
-            for y in range(3):
-                if (board[x][y] == 0):
-                    board[x][y] = -1
-                    movenum += 1
-                    value = min(value, minimax(True))
-                    board[x][y] = 0
-                    movenum -= 1
-                    if (value == -1):
-                        return value
-        return value
-    print("WTF")
+    if (getWinner() != None): return getWinner()
+    elif (movenum == 9): return 0
+    maximize = movenum % 2 == 0
+    value = -10 if maximize else 10
+    for x in range(3):
+        for y in range(3):
+            if (board[x][y] == 0):
+                board[x][y] = 1 if maximize else -1
+                movenum += 1
+                minimaxed = minimax()
+                value = max(value, minimaxed) if maximize else min(value, minimaxed)
+                board[x][y] = 0
+                movenum -= 1
+                if (value > 0 and maximize or value < 0 and not maximize): return value
+    return value
 
 def autoMove():
     global movenum
-    if (movenum == 9 or getWinner() != None):
-        return
-    bestValue = 1
-    bestX, bestY = -1, -1
+    if (movenum == 9 or getWinner() != None): return
+    maximize = movenum % 2 == 0
+    bestValue, bestX, bestY = -1 if maximize else 1 , -1, -1
     for x in range(3):
         for y in range(3):
             if (board[x][y] == 0):
                 if (bestX == -1):
                     bestX = x
                     bestY = y
-                board[x][y] = -1
+                board[x][y] = 1 if maximize else -1
                 movenum += 1
-                minimaxed = minimax(True)
+                minimaxed = minimax()
                 board[x][y] = 0
                 movenum -= 1
-                if (minimaxed < bestValue):
+                if (maximize and minimaxed > bestValue or not maximize and minimaxed < bestValue):
                     bestValue = minimaxed
                     bestX = x
                     bestY = y
-                    print("new best at {},{}".format(x,y))
-                if (bestValue == -1):
-                    move(x, y)
-                    print("-1 found at {},{}".format(x,y))
-                    return
     move(bestX, bestY)
-    print("0 found at {},{}".format(bestX,bestY))
 
-
+import tkinter
 window = tkinter.Tk('Tic Tac Toe')
-canvas = tkinter.Canvas(window, width = 300, height = 300)
+canvas = tkinter.Canvas(window, width = 600, height = 600)
 canvas.pack()
-canvas.create_line(000, 100, 300, 100)
-canvas.create_line(000, 200, 300, 200)
-canvas.create_line(100, 000, 100, 300)
-canvas.create_line(200, 000, 200, 300)
-window.maxsize(300, 300)
-window.minsize(300, 300)
-
+canvas.create_line(000, 200, 600, 200)
+canvas.create_line(000, 400, 600, 400)
+canvas.create_line(200, 000, 200, 600)
+canvas.create_line(400, 000, 400, 600)
 window.bind("<Button 1>", click)
-window.bind("<Button 2>", click)
-
 movenum = 0
-
-board = [
-    [ 1, 0, 0],
-    [ 0, 0, 0],
-    [ 0,-1, 0],
-]
-
-print(minimax(True))
-print(minimax(False))
-
-board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-]
-
+board =[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 window.mainloop()
