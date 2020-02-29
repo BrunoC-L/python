4cc1aca73b9846a7c7fbf639c51dbc1137c741d5
= <bruno.bcl@sympatico.ca>
2020-02-28 21:50:56 -0500

    i think this is fine

--- romanNumerals/yep.py
+++ romanNumerals/yep.py
@@ -1,46 +1,126 @@
-def toDecimal(romanNumeral):
-    thousandsString, romanNumeral = getThousandsString(romanNumeral)
-    hundredsString, romanNumeral = getHundredsString(romanNumeral)
+class RomanNumeralConverter:
+    def accept(self, letter):
+        times = 0
+        while (len(self.romanNumeral) > 0 and self.romanNumeral[0] == letter):
+            times += 1
+            self.romanNumeral = self.romanNumeral[1:]
+        return times
 
-def getThousandsString(romanNumeral):
-    thousandsString = ""
-    i = 0
-    length = len(romanNumeral)
-    while(i < length):
-        c = romanNumeral[i]
-        if (not "MC".__contains__(c)):
-            break
-        thousandsString += c
-        i += 1
-    return thousandsString, romanNumeral[i:]
+    def lookahead(self, letters):
+        return self.romanNumeral.find(letters) == 0
 
-def getHundredsString(romanNumeral):
-    thousandsString = ""
-    i = 0
-    length = len(romanNumeral)
-    while(i < length):
-        c = romanNumeral[i]
-        if (not "DC".__contains__(c)):
-            break
-        thousandsString += c
-        i += 1
-    return thousandsString, romanNumeral[i:]
+    def toDecimal(self, romanNumeral):
+        self.sum = 0
+        self.romanNumeral = romanNumeral
+        try:
+            self.thousands()
+            self.fiveHundreds()
+            self.hundreds()
+            self.fifties()
+            self.tens()
+            self.fives()
+            self.units()
+            print(f"{romanNumeral} = {self.sum}")
+            return self.sum
+        except Exception as err:
+            print(err)
 
-def valueAndSubstract(c):
-    if (c == "I"):
-        return 1
-    if (c == "V"):
-        return 5
-    if (c == "X"):
-        return 10
-    if (c == "L"):
-        return 50
-    if (c == "C"):
-        return 100
-    if (c == "D"):
-        return 500
-    if (c == "M"):
-        return 1000
+    def thousands(self):
+        toAdd = 1000 * self.accept("M")
+        if (self.lookahead("CM")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 900
+            if (self.lookahead("C")):
+                raise Exception("Invalid syntax while parsing thousands")
+        if (toAdd > 3900):
+            raise Exception("Invalid syntax while parsing thousands")
+        else:
+            self.sum += toAdd
+
+    def fiveHundreds(self):
+        toAdd = 500 * self.accept("D")
+        if (self.lookahead("CD")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 400
+            if (self.lookahead("C")):
+                raise Exception("Invalid syntax while parsing five hundreds")
+        if (toAdd > 500):
+            raise Exception("Invalid syntax while parsing five hundreds")
+        else:
+            self.sum += toAdd
+    
+    def hundreds(self):
+        toAdd = 100 * self.accept("C")
+        if (self.lookahead("XC")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 90
+            if (self.lookahead("X")):
+                raise Exception("Invalid syntax while parsing hundreds")
+        if (toAdd > 390):
+            raise Exception("Invalid syntax while parsing hundreds")
+        else:
+            self.sum += toAdd
+
+    def fifties(self):
+        toAdd = 50 * self.accept("L")
+        if (self.lookahead("XL")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 40
+            if (self.lookahead("X")):
+                raise Exception("Invalid syntax while parsing fifties")
+        if (toAdd > 50):
+            raise Exception("Invalid syntax while parsing fifties")
+        else:
+            self.sum += toAdd
+
+    def tens(self):
+        toAdd = 10 * self.accept("X")
+        if (self.lookahead("IX")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 9
+            if (self.lookahead("O")):
+                raise Exception("Invalid syntax while parsing tens")
+        if (toAdd > 39):
+            raise Exception("Invalid syntax while parsing tens")
+        else:
+            self.sum += toAdd
+
+    def fives(self):
+        toAdd = 5 * self.accept("V")
+        if (self.lookahead("IV")):
+            self.romanNumeral = self.romanNumeral[2:]
+            toAdd += 4
+            if (self.lookahead("I")):
+                raise Exception("Invalid syntax while parsing fives")
+        if (toAdd > 5):
+            raise Exception("Invalid syntax while parsing fives")
+        else:
+            self.sum += toAdd
+
+    def units(self):
+        toAdd = self.accept("I")
+        if (toAdd > 3):
+            raise Exception("Invalid syntax while parsing units")
+        else:
+            self.sum += toAdd
+        if (len(self.romanNumeral) > 0):
+            raise Exception("reached end of parsing order and there were digits remaining")
+
+    def valueAndSubstract(self, c):
+        if (c == "I"):
+            return 1
+        if (c == "V"):
+            return 5
+        if (c == "X"):
+            return 10
+        if (c == "L"):
+            return 50
+        if (c == "C"):
+            return 100
+        if (c == "D"):
+            return 500
+        if (c == "M"):
+            return 1000
 
 tests = [
     "MDCLXVI", 1666,
@@ -64,9 +144,8 @@ tests = [
     "XXII", 22,
     "XXIII", 23,
     "MCDLVI", 1456,
-    "MCDVLI", 1446,
+    "MCDXLI", 1441,
     "MCDLI", 1451,
-    "MLDXI", 1,
 
     "MXXC", None,
     "XVX", None,
@@ -80,7 +159,5 @@ tests = [
     "VV", None,
 ]
 
-toDecimal("MCMLLC")
-
-# for i in range(int(len(tests) / 2)):
-#     assert(evaluate(tests[2 * i]) == tests[2 * i + 1])
+for i in range(int(len(tests) / 2)):
+    assert(RomanNumeralConverter().toDecimal(tests[2 * i]) == tests[2 * i + 1])
